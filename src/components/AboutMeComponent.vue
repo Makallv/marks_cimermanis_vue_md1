344f67fd8443ea421c23763103b8f3ec51b458bb
MakalFTW <112897751+MakalFTW@users.noreply.github.com>
2023-10-18 21:28:51 +0300

    Vue part 1

---
+++ src/components/AboutMeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>ABOUT ME</h1>
+    <div class="about">
+      <p>Name:</p><h3> {{ user.name }}</h3>
+    </div>
+    <div class="about">
+      <p>Surname:</p><h3> {{ user.surname }}</h3>
+    </div>
+    <div class="about">
+      <p>Code:</p><h3> {{ user.code }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Marks",
+        surname: "Cimermanis",
+        code: "IT21060",
+      },
+    };
+  },
+};
+</script>
